11091d5338cb0d3215dce4d289eed55939d40e84
Yusuf0403 <adeyemiyusuf48@gmail.com>
2022-04-10 13:53:49 -0700

    Said

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,6 +8,7 @@
  */
 int main(void)
 {
+
 int c = 0;
 int f_d;
 int l_d;
@@ -19,10 +20,8 @@ while (c <= 98)
 f_d = (c / 10 + '0');
 l_d = (c % 10 + '0');
 c2 = 0;
-while (c2 <= 99){
-
+while (c2 <= 99)
 {
-
 f_d2 = (c2 / 10 + '0');
 l_d2 = (c2 % 10 + '0');
 if (c < c2)
@@ -37,14 +36,10 @@ if (c != 98)
 putchar(',');
 putchar(' ');
 }
-
-
 }
 c2++;
-
 }
 c++;
-
 }
 putchar('\n');
 return (0);
