d332260dd5cfe1ba434db07d5ac8af9c8dfc9889
Yusuf0403 <adeyemiyusuf48@gmail.com>
2022-04-10 10:39:31 -0700

    Kabir

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 /**
-
  * main - main block
-
  * Description: Get a random number and check its last digit, compare it with 5
 
  * Return: 0
@@ -33,3 +29,4 @@ putchar('\n');
 return (0);
 
 }
+
