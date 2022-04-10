18e2222b8a22af343829d5cea67afb9873796010
Yusuf0403 <adeyemiyusuf48@gmail.com>
2022-04-10 04:20:51 -0700

    Kiekie

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,47 @@
-#​include​ ​<​stdio.h​> 
-  
- ​/​ 
- ​ * main - prints the alphabet in lowercase, and then in uppercase, 
- ​ * followed by a new line 
- ​ * Return: Always 0 (Success) 
- ​ ​*/ 
- ​int​ ​main​(​void​) 
- ​{ 
- ​        ​int​ ch; 
-  
- ​        ​for​ (ch = ​'​a​'​; ch <= ​'​z​'​; ch++) 
- ​                ​putchar​(ch); 
- ​        ​for​ (ch = ​'​A​'​; ch <= ​'​Z​'​; ch++) 
- ​                ​putchar​(ch); 
- ​        ​putchar​(​'​\n​'​); 
- ​        ​return​ (​0​);
+#include <stdio.h>
+
+/**
+
+ * main - main block
+
+ * Description:  prints the alphabet in lowercase,
+
+ * and then in uppercase, followed by a new line.
+
+ * Return: 0
+
+ */
+
+int main(void)
+
+{
+
+char c = 'a';
+
+while (c <= 'z')
+
+{
+
+putchar(c);
+
+c++;
+
+}
+
+c = 'A';
+
+while (c <= 'Z')
+
+{
+
+putchar(c);
+
+c++;
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
