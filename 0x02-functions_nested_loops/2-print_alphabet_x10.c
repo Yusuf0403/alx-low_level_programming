856dadee95f86056835848d2580d65f4e3e72152
Yusuf0403 <adeyemiyusuf48@gmail.com>
2022-04-13 15:49:09 -0700

    Benz

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,15 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10 - prints alphabet 10 times
+ * print_a
+lphabet_x10 - prints alphabet 10 times
  * Return:void
  */
 void print_alphabet_x10(void)
 {
 	char c;
 	int i = 0;
+rmrm
 	while (i < 10)
 	{
 		c = 'a';
