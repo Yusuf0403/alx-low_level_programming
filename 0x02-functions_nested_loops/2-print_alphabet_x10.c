e9561985eeba1b0fd601287bec28ab43df1034f7
Yusuf0403 <adeyemiyusuf48@gmail.com>
2022-04-13 16:11:56 -0700

    Tobi

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,17 @@
  */
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
-	while (i < 10)
-	{
-		c = 'a';
-		while (c <= 'z')
-		{
-			_putchar(c);
-			c++;
-		}
-		_putchar('\n');
-		i++;
-	}
-
+char c;
+int i = 0;
+while (i < 10)
+{
+c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+i++;
+}
 }
