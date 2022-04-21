bbde88d993d32914140f5d313fac23325903ad10
Yusuf0403 <adeyemiyusuf48@gmail.com>
2022-04-21 13:59:31 -0700

    Muhibah

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
+
 /**
- * _islower - function
- * @c: character to check case sensitiviy
- * Return: Always 0.
+ * _islower - check for lower case letter
+ * @c : character to check the case
+ * Return:0 or 1
  */
+
 int _islower(int c)
 {
-if (c >= 97 && c <= 122)
-{
-return (1);
-}
-return (0);
+
+	return (c >= 97 && c <= 122);
+
 }
