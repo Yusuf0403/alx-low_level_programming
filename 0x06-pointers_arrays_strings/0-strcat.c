7e4e9754073e8bb695dca097513295ce6e091d15
Yusuf0403 <adeyemiyusuf48@gmail.com>
2022-04-23 10:26:11 -0700

    Ade

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * _strcat - concatenates two strings
- * @dest: input parameter string
+ * _strcat - concatenates two strings
+ * @src: input parameter string
  * @src: input parameter string
  *
  * Return: dest
@@ -12,13 +13,14 @@ char *_strcat(char *dest, char *src)
 	int b;
 	
 	a = 0;
-
+	
 	while (dest[a] != 0)
 	{
 		a++;
 	}
+	
 	b = 0;
-
+	
 	while (src[b] != 0)
 	{
 		dest[a] = src[b];
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,19 +1,16 @@
-#include "main.h"
-#include <stdio.h>
-
-/*
- * main - check the code
- *
- * Return: Always 0.
-*/
-int main(void)
-{
-	int n;
-
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-
-	return (0);
-}
+#ifndef MAIN_H
+#define MAIN_H
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
+char *rot13(char *str);
+void print_number(int n);
+int _putchar(char c);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+#endif /MAIN_H/
