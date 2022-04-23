de6353db755068483bc4dea1bece51a6b1be9c80
Yusuf0403 <adeyemiyusuf48@gmail.com>
2022-04-23 09:43:47 -0700

    Keji

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings * _strcat - concatenates two strings
+ * _strcat - concatenates two strings
  * @dest: input parameter string
  * @src: input parameter string
  *
@@ -10,14 +10,13 @@ char *_strcat(char *dest, char *src)
 {
 	int a;
 	int b;
-
+	
 	a = 0;
 
 	while (dest[a] != 0)
 	{
 		a++;
 	}
-	
 	b = 0;
 
 	while (src[b] != 0)
