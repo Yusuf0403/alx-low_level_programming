bf3d1e353dea7fde88ed6d13d0a4bb12d17d98d1
Yusuf0403 <adeyemiyusuf48@gmail.com>
2022-04-24 12:51:09 -0700

    Toyosi

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,7 +11,7 @@
 void print_buffer(char *b, int size)
 {
 	int o, j, i;
-	
+
 	o = 0;
 
 	if (size <= 0)
@@ -26,3 +26,26 @@ void print_buffer(char *b, int size)
 		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
+}
+
